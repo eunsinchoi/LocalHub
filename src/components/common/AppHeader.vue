222feecb5953c8bafc7eccc601bc7e4f8b9e6b36
Eunsin <eunsin_choi@naver.com>
2026-07-15 14:09:57 +0900

    홈화면 버튼 경로 수정

--- src/components/common/AppHeader.vue
+++ src/components/common/AppHeader.vue
@@ -1,14 +1,6 @@
 <script setup>
 import seoulLogo from '../../seoul_logo.png'
-
-const categories = [
-  { name: '관광지', path: '/board/tourist' },
-  { name: '레포츠', path: '/board/leports' },
-  { name: '문화시설', path: '/board/culture' },
-  { name: '쇼핑', path: '/board/shopping' },
-  { name: '여행코스', path: '/board/course' },
-  { name: '축제공연', path: '/board/festival' },
-]
+import { categories } from '../../constants/categories.js'
 </script>
 
 <template>
